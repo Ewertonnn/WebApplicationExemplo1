0d2c2965d8e2b5be722a027513758c9c8f3fdebf
Administrador <Administrador@CST30LAB202D009>
2023-09-01 17:51:37 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContex.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PageEstudo.Data{
+    public class ApplicationDbContex : DbContext{
+
+        public ApplicationDbContex(DbContextOptions<ApplicationDbContex> options) : base(options) { 
+
+            PublicKey DbSet<Produto> Produto { get; Set(); } = default!;
+        
+        }
+
+    }
+}
